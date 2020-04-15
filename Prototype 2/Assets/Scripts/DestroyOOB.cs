0ed80a1338706a753c73bd69587519ac18ba6fd3
Arttu Pylkkö <rudezki@gmail.com>
2020-04-15 15:55:33 +0300

    Initial commit

---
+++ Prototype 2/Assets/Scripts/DestroyOOB.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOOB : MonoBehaviour
+{
+    public float topBound = 30;
+    public float bottomBound = -10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If an object goes past the screen, this will destroy it
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        } else if (transform.position.z < bottomBound)
+        {
+            Debug.Log("Game over!");
+            Destroy(gameObject);
+        }
+    }
+}
