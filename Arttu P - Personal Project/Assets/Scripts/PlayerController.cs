b7ae23b62459685be2d9256bcf8a6a9d3ca3e5d4
Arttu Pylkkö <rudezki@gmail.com>
2020-04-21 10:56:24 +0300

    Unit 3 done

---
+++ Arttu P - Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public float horizontalSpeed;
+    public float verticalSpeed;
+    private float zBoundary = 10.0f;
+    private float xBoundary = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        PlayerBoundary();
+  
+    }
+    //This moves player
+    void MovePlayer()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * horizontalSpeed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * verticalSpeed);
+    }
+    //This determines the boundaries of the player
+    void PlayerBoundary()
+    {
+        if (transform.position.z < -zBoundary)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBoundary);
+        }
+        if (transform.position.z > zBoundary)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBoundary);
+        }
+        if (transform.position.x < -xBoundary)
+        {
+            transform.position = new Vector3(-xBoundary, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xBoundary)
+        {
+            transform.position = new Vector3(xBoundary, transform.position.y, transform.position.z);
+        }
+    }
+}
